41e5ecd6909129624f05dc5e40f77ebba4b4860b
Kyrill van Fessem <kyrillvanfessem@hotmail.nl>
2023-12-20 08:55:58 +0100

    added redering and first template feature

--- src/Template.php
+++ src/Template.php
@@ -5,7 +5,7 @@ use Stagaires\PhpTemplate\Exception\TemplateNotFoundException;
 
 class Template
 {
-    protected string $path;
+    private string $path;
 
     public function __construct($path)
     {
@@ -15,12 +15,47 @@ class Template
     /**
      * @throws TemplateNotFoundException
      */
-    public function render(string $template, array $data =[])
+    public function render(string $template, array $data = []): string
     {
-        $path = $this->path . DIRECTORY_SEPARATOR . $template . '.php';
+        $path = $this->getTemplatePath($template);
+        $this->validateTemplate($path);
 
-        if(! file_exists($path)) {
-            throw TemplateNotFoundException::make($template);
+        $content = $this->getTemplateContent($path);
+        $content = $this->modifyTemplateContent($content);
+
+        return $this->renderTemplate($content, $data);
+    }
+
+    private function getTemplatePath(string $template): string
+    {
+        return $this->path . DIRECTORY_SEPARATOR . $template . '.php';
+    }
+
+    /**
+     * @throws TemplateNotFoundException
+     */
+    private function validateTemplate(string $path): void
+    {
+        if (!file_exists($path)) {
+            throw TemplateNotFoundException::make($path);
         }
+    }
+
+    private function getTemplateContent(string $path): string
+    {
+        return file_get_contents($path);
+    }
+
+    private function modifyTemplateContent(string $content): string
+    {
+        return str_replace('{{', '<?= ', str_replace('}}', ' ?>', $content));
+    }
+
+    private function renderTemplate(string $content, array $data): string
+    {
+        extract($data);
+        ob_start();
+        require $content;
+        return ob_get_clean();
     }
 }
